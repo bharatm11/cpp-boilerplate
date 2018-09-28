92b773452ce7a6ea1846125ae307aec8c4b9dae2
bharatm11 <bharat.mathur@hotmail.com>
2018-09-27 20:00:04 -0400

    Initialized terminator in main.cpp. deleted results in AnalogSensor.cpp

--- app/AnalogSensor.cpp
+++ app/AnalogSensor.cpp
@@ -2,8 +2,8 @@
 #include <numeric>
 #include <vector>
 
-AnalogSensor::AnalogSensor(unsigned int samples)
-    : mSamples(samples)
+AnalogSensor::AnalogSensor(unsigned int samples=0)
+: mSamples(samples)
 {
 }
 
@@ -13,10 +13,9 @@ AnalogSensor::~AnalogSensor()
 
 int AnalogSensor::Read()
 {
-    std::vector<int> *readings = new std::vector<int>(mSamples, 10);
+  std::vector<int> *readings = new std::vector<int>(mSamples, 10);
 
-    double result = std::accumulate( readings->begin(), readings->end(), 0.0 ) / readings->size();
-    return result;
+  double result = std::accumulate( readings->begin(), readings->end(), 0.0 ) / readings->size();
+  delete readings;
+  return result;
 }
-
-
